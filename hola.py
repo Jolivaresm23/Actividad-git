e617bc01168c6629fae2138b35a3c0b9d9ae81f8
hirijo no c <jolivaresm@estudiante.uc.cl>
2026-05-14 17:36:12 -0400

    Arreglé un error en el archivo py

--- hola.py
+++ hola.py
@@ -4,7 +4,7 @@ salto = randint(0,10)
 resta = randint(0,10)
 if resta == salto: 
     print("Te rompiste la pierna")
-elif salto - resta >= 0:
+elif salto - resta > 0:
     print(f"Saltaste {salto - resta} metros")
 else:
     print(f"Saltaste {salto - resta} metros, rompiste las leyes de la fisica")
