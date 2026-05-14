63f90436370d6e824d565e95c4479a37e43e335d
hirijo no c <jolivaresm@estudiante.uc.cl>
2026-05-14 16:50:08 -0400

    Todavia no me deja hacer push, intento subir py

---
+++ hola.py
@@ -0,0 +1,10 @@
+from random import *
+
+salto = randint(0,10)
+resta = randint(0,10)
+if resta == salto: 
+    print("Te rompiste la pierna")
+elif salto - resta >= 0:
+    print(f"Saltaste {salto - resta} metros")
+else:
+    print(f"Saltaste {salto - resta} metros, rompiste las leyes de la fisica")
